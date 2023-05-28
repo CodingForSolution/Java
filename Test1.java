9c63c04f482ac89f46558658fa7d9d9200b1c8ce
CodingForSolution <134803910+CodingForSolution@users.noreply.github.com>
2023-05-28 17:03:38 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,1 +1,11 @@
+import java.util.*;
+public class Test{
+	public static void main(String args[]){
+		System.out.println("Main function loaded.........");
+		System.out.println("Printing args[] values");
+		for(int i=0;i<args.length;i++){
+			System.out.println(args[i]);
+		}
+	}
 
+}
